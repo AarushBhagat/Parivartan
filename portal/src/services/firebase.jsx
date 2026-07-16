c0ad516e8f68b34a6e5084ed069925c9e75e8a70
AarushBhagat <aarushbhagat093@gmail.com>
2026-07-16 10:50:43 +0530

    Fixing the Api security

--- portal/src/services/firebase.jsx
+++ portal/src/services/firebase.jsx
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCXjKyjjpIzfViaPwqNaFm8h6d_Oqs4ybY",
-  authDomain: "parivartan-12.firebaseapp.com",
-  projectId: "parivartan-12",
-  storageBucket: "parivartan-12.firebasestorage.app",
-  messagingSenderId: "779445083679",
-  appId: "1:779445083679:web:9ad7ea10183628beb4bf2a",
-  measurementId: "G-J1E4JKGFG5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
